5752db1db1633a2080b23e8c7a05c68f3b9f1eb5
cxc61cxc <t4950c@gmail.com>
2021-08-10 03:56:11 +0200

    Update settings.py

--- carrara/carrara/settings.py
+++ carrara/carrara/settings.py
@@ -165,9 +165,9 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 't4950c@gmail.com'
-EMAIL_HOST_PASSWORD = '@P1r1ll0'
-DEFAULT_FROM_EMAIL = 'dallo staff <t4950c@gmail.com>'
+EMAIL_HOST_USER = 'xxxxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxx'
+DEFAULT_FROM_EMAIL = 'dallo staff <xxxxxxx@gmail.com>'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
