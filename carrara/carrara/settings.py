9df58be73ef51e13287bfbd1b8623f3f39b8a224
cxc61cxc <t4950c@gmail.com>
2021-08-10 07:26:01 +0200

    Update settings.py

--- carrara/carrara/settings.py
+++ carrara/carrara/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mmsofg$gxriye#ns@q!bk*olvb%ql3zki951p-ngsuwh!z%nh!'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
